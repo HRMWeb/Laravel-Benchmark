aa210c63a96123e0c1ad0f52823e746cdb9b3a1d
Michael Alexander <mick@hrmweb.com.au>
2013-08-07 11:07:11 +1000

    Removed redundant imports.

--- app/models/Bench.php
+++ app/models/Bench.php
@@ -1,8 +1,5 @@
 <?php
 
-use Illuminate\Auth\UserInterface;
-use Illuminate\Auth\Reminders\RemindableInterface;
-
 class Bench extends Eloquent {
 	public $timestamps = false;
 	protected $table = 'bench';
